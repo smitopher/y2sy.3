e3e4e1578d3faf7c28e5bbd6ecb302da6855f87c
Chris Smith <chris.smith@cmfirstgroup.com>
2020-05-08 12:44:59 -0500

    First attempt at reactive API

--- src/main/java/com/cmfirsttech/y2/api/resource/Y2ListAsync.java
+++ src/main/java/com/cmfirsttech/y2/api/resource/Y2ListAsync.java
@@ -1,5 +1,31 @@
 package com.cmfirsttech.y2.api.resource;
 
+import javax.annotation.security.RolesAllowed;
+import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import com.cmfirsttech.y2.api.model.impl.ObjectListEntry;
+import com.cmfirsttech.y2.api.service.impl.Y2ListServiceAsync;
+
+import io.smallrye.mutiny.Multi;
+
+@Path("/y2ListAsync")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+@RolesAllowed(value = "y2api")
 public class Y2ListAsync {
+	
+	@Inject
+	Y2ListServiceAsync asyncService;
+	
+	@GET
+	@Path("/all")
+	public Multi<ObjectListEntry> all() {
+		return asyncService.asyncAll();
+	}
 
 }
--- src/main/java/com/cmfirsttech/y2/api/service/impl/Y2ListServiceAsync.java
+++ src/main/java/com/cmfirsttech/y2/api/service/impl/Y2ListServiceAsync.java
@@ -1,23 +1,33 @@
 package com.cmfirsttech.y2.api.service.impl;
 
+import java.util.stream.Stream;
+
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
 
+import com.cmfirsttech.y2.api.entity.impl.Y2ModelListEntry;
+import com.cmfirsttech.y2.api.mapper.CoreMapper;
 import com.cmfirsttech.y2.api.model.impl.ObjectListEntry;
-import com.cmfirsttech.y2.api.model.internal.ModelObjectList;
 
 import io.smallrye.mutiny.Multi;
-import io.smallrye.mutiny.Uni;
 
 @ApplicationScoped
 public class Y2ListServiceAsync {
 	
 	@Inject
-	CoreService service;
+	CoreMapper mapper;
 	
-	Multi<ObjectListEntry> asyncAll() {
-		// Multi.createBy().combining().
-		
-		return null;
+	public Multi<ObjectListEntry> asyncAll() {
+		return Multi.createFrom().items(getStream()).map(this::map);
+	}
+	
+	private Stream<Y2ModelListEntry> getStream() {
+		return Y2ModelListEntry.streamAll();
+	}
+	
+	private ObjectListEntry map(Y2ModelListEntry y) {
+		ObjectListEntry listEntry = new ObjectListEntry();
+		mapper.directMap(listEntry, y);
+		return listEntry;
 	}
 }
